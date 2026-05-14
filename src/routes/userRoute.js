ca50f507ff8ce0a411906a525987726dba2bb855
Rudrajit Saha Roy <rudrajitsaharoy24@gmail.com>
2026-05-14 12:48:59 +0530

    Added validators

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,10 +2,11 @@ import express from 'express'
 import { login, logout, userRegister } from '../controllers/userController.js'
 import { verification } from '../middleware/verifyToken.js'
 import {hasToken} from '../middleware/hasToken.js'
+import { userValidateSchema, validateUser } from '../validators/userValidate.js'
 
 export const userRoute = express.Router()
 
-userRoute.post('/register', userRegister)
+userRoute.post('/register', validateUser(userValidateSchema), userRegister)
 userRoute.get('/verify', verification)
 userRoute.post('/login', login)
 userRoute.delete('/logout',hasToken , logout)
